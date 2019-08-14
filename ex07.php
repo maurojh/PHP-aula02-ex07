960e6e94cb0fcb17045a75fa5a6ad294fbe35f21
Mauro <maurojh@gmail.com>
2019-08-13 22:07:04 -0300

    first commit

---
+++ ex07.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Dados</title>
+</head>
+<body>
+<form action="recebe07.php">
+    Sexo: <br>
+         <input type="radio" name="sexo" value="m">Masculino
+         <br>
+          <input type="radio" name="sexo" value="f">Feminino
+          <br>
+    Idade: <input type="number" name="idade" min="18"><br>
+    Bebida: <br>
+    <input type="checkbox" name="cerveja">Cerveja <br>
+    <input type="checkbox" name="refrigerante">Refrigerante <br>
+        <input type="submit" value="Enviar">
+</form>
+</body>
+</html>
